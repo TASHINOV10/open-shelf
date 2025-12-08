03bf5c9566182ceac8eaa27e8fe6a23bb1639c20
TASHINOV10 <tasinovilian@gmail.com>
2025-12-08 02:55:48 +0200

    temporary change to cors settings

--- server/main.py
+++ server/main.py
@@ -21,7 +21,7 @@ app = FastAPI(title=settings.app_name, debug=settings.debug)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.allowed_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
