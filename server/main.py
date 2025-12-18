94e3ea8c1fcd9e280ca4ee8be6dbc8a13b407cde
TASHINOV10 <tasinovilian@gmail.com>
2025-12-18 16:18:47 +0200

    configured for local dev

--- server/main.py
+++ server/main.py
@@ -23,8 +23,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://open-shelf-frontend-793683179596.europe-central2.run.app",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://192.168.1.178:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
