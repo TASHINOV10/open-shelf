e523d35f47a52c4e13e6f4dfc4d2127519484376
TASHINOV10 <tasinovilian@gmail.com>
2026-01-29 22:52:17 +0200

    - receipts model updated - now recording uploaded timestamp data

--- server/models/receipts.py
+++ server/models/receipts.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, Date, ForeignKey, String
+from sqlalchemy import Column, Integer, Date, ForeignKey, String, DateTime
+from sqlalchemy.sql import func
 from ..database.database import Base
 
 class Receipts(Base):
@@ -7,4 +8,5 @@ class Receipts(Base):
     pk_id = Column(Integer, primary_key=True, index=True)
     external_id = Column(String, unique=True, index=True, nullable=False)
     post_date = Column(Date, nullable=True)
+    uploaded_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     fk_store_id = Column(Integer, ForeignKey("stores.pk_id"))
